b71ad7ca9da71a9dcc045aa1d5b73c2e455c44bf
DiiLord <d.drodt@gmx.de>
2016-03-24 18:53:06 +0100

    Titel hinzugefügt

--- Base/index.php
+++ Base/index.php
@@ -8,12 +8,13 @@
 <meta name="description" content="Die Internetseite der Bürgerinitiative Lausböhl Groß-Gerau">
 <meta name="keywords" content="Lausböhl, Groß-Gerau, Bürgerinitiative Lausböhl">
 <meta http-equiv="content-type" content="text/html"; charset="utf-8">
+    <title>B&uuml;rgerinititaive Lausb&ouml;hl</title>
 <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="/style/lbv2.css">
     <link rel="stylesheet" type="text/css" href="style/content.css">
 </head>
     <header>
-    
+        <div class="title">Bürgerinitiative Lausböhl</div>
     </header>
     <main>
     
